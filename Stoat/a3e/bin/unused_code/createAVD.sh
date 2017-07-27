4098ed6a4ebf6469eab6b588af866133c9535ba6
tingsu <suting1989@gmail.com>
2017-07-27 11:55:06 +0800

    remove files

--- Stoat/a3e/bin/unused_code/createAVD.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-# this script will remove the crashed/old avd, and create a new one
-
-avd_name=$1
-
-rm -rf ~/.android/avd/${avd_name}.*
-android create avd -n ${avd_name} -t android-16 -s HVGA -c 128M --abi armeabi-v7a -d "Nexus 7"
